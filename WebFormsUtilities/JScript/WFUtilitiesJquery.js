abbe7018ab3fec0b52c523f1c17fa84a5a2b29a2
aikeru_cp <SND\aikeru_cp>
2012-03-16 20:37:17 +0000

    Added XML comments to WFUtilitiesJquery.js

--- WebFormsUtilities/JScript/WFUtilitiesJquery.js
+++ WebFormsUtilities/JScript/WFUtilitiesJquery.js
@@ -2,6 +2,11 @@
 
 // WFUtilities
 function WFSubmitForm(methodName) {
+    /// <summary>Invoke a full postback, passing an extra JSMethod form variable to indicate which method the server should invoke.
+    /// &#10;You can use WFPageUtilities.CallJSMethod() on the server side during a postback to call this method automatically.
+    /// &#10;The target method should have two arguments: object sender and EventArgs e, which will both be null.</summary>
+    /// <param name="methodName" type="String">The name of the method on the page class to invoke.</param>
+    /// <returns>Returns false.</returns>
     var elem = $('#JSMethod');
     var elemStr = '<input type="hidden" name="JSMethod" id="JSMethod" value="' + methodName + '" />';
     if (elem[0] === undefined) {
@@ -14,14 +19,23 @@ function WFSubmitForm(methodName) {
 }
 
 function WFEnableUpload() {
+    ///<summary>Add enctype and encoding attributes to the &lt;form&gt; tag so that WebForms will see the files uploaded by &lt;input type=file&gt;</summary>
     $('form').attr("enctype", "multipart/form-data");
     $('form').attr("encoding", "multipart/form-data");
 }
+
 
 //Syntax is like this:
 //  PageName.aspx/MethodName
 //This works against AJAX in 3.5 but not 2.0 (2.0 has no .d property)
 function WFCallPage(pageMethodURL, args) {
+    ///<summary>Shortcut to invoke a [WebMethod] using jQuery's $.ajax()
+    ///&#10;[WebMethod]'s from any WebForms page can be called.</summary>
+    ///<param name="pageMethodURL" type="String">Syntax: PageName.aspx/WebMethodName</param>
+    ///<param name="args" type="Object">Magic arguments object.
+    /// &#10;{data: a STRING in the form of a JSON object which must match the signature of the webmethod
+    /// &#10;success: function(a, b, c),
+    /// &#10;error: function(a, b, c)}</param>
     if (args.data === undefined || args.data === null || args.data === "") { args.data = "{}"; }
     $.ajax({ type: "POST", url: pageMethodURL, data: args.data, contentType: "application/json; charset=utf-8",
         dataType: "json",
@@ -41,6 +55,8 @@ function WFCallPage(pageMethodURL, args) {
 //Uses jQuery's .serialize() method and excludes ASP.net's extra hidden fields
 //Returns what looks like a raw post body that is URL encoded (ie: name=value&name2=value2...) and has double-quotes escaped " -> \"
 function WFSerializeForm() {
+    ///<summary>Serialize the form into a post 'body' string. (ie: name=value&amp;name2=value2...) Will escape double-quotes.</summary>
+    ///<returns>Returns a string in the form of (ie: name=value&amp;name2=value2...)</returns>
     var serStr = $('form :input').not('#__VIEWSTATE,#__EVENTVALIDATION').serialize();
     serStr = serStr.replace(/"/g, "\\\"");
     return serStr;
